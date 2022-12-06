abef4028748de0d824fc58607b2adf377096e650
Vadim Khadkevich <vadimkhadkevich@gmail.com>
2022-12-06 16:26:47 +0200

    services configuration

--- FootballStats.Application/Program.cs
+++ FootballStats.Application/Program.cs
@@ -2,6 +2,7 @@ using FootballStats.ApplicationModule;
 using FootballStats.ApplicationModule.Common.Interfaces;
 using FootballStats.ApplicationModule.Common.Interfaces.Repositories;
 using FootballStats.Domain.Entities;
+using FootballStats.Infrastructure;
 using FootballStats.Infrastructure.Authentication;
 using FootballStats.Infrastructure.Logging;
 using FootballStats.Infrastructure.Persistence;
@@ -20,51 +21,14 @@ builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
 builder.Host.UseNLog();
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSingleton<IUriService>(o =>
-{
-    var accessor = o.GetRequiredService<IHttpContextAccessor>();
-    var request = accessor.HttpContext?.Request;
-    var uri = string.Concat(request?.Scheme, "://", request?.Host.ToUriComponent());
-    return new UriService(uri);
-});
 
 // Add services to the container.
-builder.Services.AddScoped<IApplicationDbContext, FootballStatsDbContext>();
-builder.Services.AddScoped<IAuthentication, ApplicationAuthentication>();
-builder.Services.AddScoped<ISignUpRepository, SignUpRepository>();
-builder.Services.AddScoped<ILoginRepository, LoginRepository>();
-builder.Services.AddScoped<IPlayersRepository, PlayersRepository>();
-builder.Services.AddScoped<ITrainingsRepository, TrainingsRepository>();
-builder.Services.AddScoped<ICoachesRepository, CoachesRepository>();
-
-builder.Services.AddScoped<ISortHelper<Player>, SortHelper<Player>>();
-builder.Services.AddScoped<ISortHelper<Training>, SortHelper<Training>>();
-builder.Services.AddScoped<ISortHelper<Coach>, SortHelper<Coach>>();
-
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = builder.Configuration["Authentication:Domain"]
-            + $"{builder.Configuration["FootballStatsAuthentication:Domain"]}/";
-        options.Audience = builder.Configuration["Authentication:Audience"]
-            + $"{builder.Configuration["FootballStatsAuthentication:Audience"]}/";
-    });
-string str = builder.Configuration.GetConnectionString("FootballStatsDBConnection") +
-        $"{builder.Configuration["FootballStatsDBConnection:Password"]};";
-builder.Services.AddDbContext<FootballStatsDbContext>(opt => opt.UseSqlServer(
-    builder.Configuration.GetConnectionString("FootballStatsDBConnection") +
-        $"{builder.Configuration["FootballStatsDBConnection:Password"]};",
-            options => options.EnableRetryOnFailure()
-));
-
-builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers().AddNewtonsoftJson(services =>
         services.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
 
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
